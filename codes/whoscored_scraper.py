55a5be189e722f0d695197e8590dfe491d7d6d5a
mai <supaseth@gmail.com>
2021-01-01 17:03:16 +0900

    Add scraping script for whoscored.com

---
+++ codes/whoscored_scraper.py
@@ -0,0 +1,52 @@
+# %%
+import time
+import json
+import time
+import pickle
+
+from selenium import webdriver
+from bs4 import BeautifulSoup, Comment
+
+extraction_dict = {
+    0: {'split_txt': 'var matchCentreData = ',
+        'type': 'json'},
+    1: {'split_txt': 'var matchCentreEventTypeJson = ',
+        'type': 'json'},
+    2: {'split_txt': 'var matchId = ',
+        'type': 'int'},
+    3: {'split_txt': 'var formationIdNameMappings = ',
+        'type': 'json'},
+    4: {'split_txt': None,
+        'type': 'terminate'},
+}
+# %%
+url = 'www.yoururl.com/whatever/you/want'
+
+driver = webdriver.Firefox(executable_path='/tmp/geckodriver')
+driver.get(url)
+
+soup = BeautifulSoup(driver.page_source, features='html.parser')
+time.sleep(10)
+driver.quit()
+
+# %%
+data = soup.find('div', {'id': 'multiplex-parent'})
+data = data.find_next('script', {'type': 'text/javascript'})
+
+# %%
+lst = []
+for idx in range(len(str(data).split(';'))):
+    extractor = extraction_dict.get(idx)
+    item = str(data).split(';')[idx]
+    item = item.split(extractor['split_txt'])[-1]
+    if extractor['type'] == 'json':
+        lst.append(json.loads(item))
+    elif extractor['type'] == 'int':
+        lst.append(int(item[:-1]))
+    else:
+        break
+# %%
+save_file_path = 'path/to/file.pkl'
+with open(save_file_path, 'wb') as f:
+    pickle.dump(lst, f, protocol=4)
+# %%
